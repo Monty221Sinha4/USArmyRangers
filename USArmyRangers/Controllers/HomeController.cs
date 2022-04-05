4bff8cc62b3e40e615c78278383e11c594153e4e
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-04-05 11:05:32 +0100

    I have added the controller

--- USArmyRangers/Controllers/HomeController.cs
+++ USArmyRangers/Controllers/HomeController.cs
@@ -9,7 +9,11 @@ namespace USArmyRangers.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Life()
         {
             return View();
         }
